56dd047421039b46f6aeaa940e5bbfa062045aff
venkat1230 <40557158+venkat1230@users.noreply.github.com>
2023-10-04 14:31:47 +0530

    Update Last_played.py

--- Last_played.py
+++ Last_played.py
@@ -1,10 +1,12 @@
-lastplayed = 4 #got the item from get request
+lastplayed = 6 #got the item from get request
 Played_list = [1,2,3]
 if lastplayed in Played_list:
     exit()
 else : 
 	Played_list.append(lastplayed)
-	Played_list.pop(0)
+	if 	len(Played_list) > 3 :
+            Played_list.pop(0)
 
-print (Played_list)
- 
+print(Played_list)
+
+print(len(Played_list))
